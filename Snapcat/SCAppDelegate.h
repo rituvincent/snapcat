c4431eb07da5e2a541bc0e518bf7b88fe5415bf5
Ritu Vincent <ritu@ritu-mba.corp.dropbox.com>
2014-07-14 11:33:12 -0700

    Initial Commit

---
+++ Snapcat/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  Snapcat
+//
+//  Created by Ritu Vincent on 7/14/14.
+//  Copyright (c) 2014 Ritu Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
